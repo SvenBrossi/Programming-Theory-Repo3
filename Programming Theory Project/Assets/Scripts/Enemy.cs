89edd3b1875153905d37a051ef72be639d96bcdd
SvenBrossi <85752184+SvenBrossi@users.noreply.github.com>
2021-08-22 13:12:50 +0200

    Enemy Script
    I started to develop the enemy script and added a rigidbody to the enemy object.

---
+++ Programming Theory Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //Move logic
+    virtual protected void MoveEnemy()
+    {
+
+    }
+
+    //Figure out if object collided with something
+    virtual protected void CollisionDetection()
+    {
+
+    }
+
+    //Do something once it collided
+    virtual protected void EnemyAction()
+    {
+
+    }
+}
