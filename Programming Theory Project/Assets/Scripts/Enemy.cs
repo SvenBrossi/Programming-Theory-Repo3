bb05a1ed9347c588091631fb33d8cc8c005dfe84
SvenBrossi <85752184+SvenBrossi@users.noreply.github.com>
2021-08-22 19:44:29 +0200

    Basic Enemy Move method implemented
    Work in progress.
    
    Enemy Script WIP 2
    
    Enemy Script WIP 3
    
    Now working but needs to be refind.
    
    Enemy Move parent implemented
    
    Now the basic enemy move works.

--- Programming Theory Project/Assets/Scripts/Enemy.cs
+++ Programming Theory Project/Assets/Scripts/Enemy.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float speed = 3;
+
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+    void Start()
+    {
+        enemyRb = gameObject.GetComponent<Rigidbody>();
+        player = GameObject.Find("Character");
+    }
+
+
+    void Update()
+    {
+        MoveEnemy();
+    }
+
     //Move logic
     virtual protected void MoveEnemy()
     {
-
+        Vector3 target = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(target * speed);
     }
+
 
     //Figure out if object collided with something
     virtual protected void CollisionDetection()
